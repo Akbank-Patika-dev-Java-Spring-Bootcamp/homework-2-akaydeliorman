878398224cd82ffa30b379bf4fbec309528168a8
akaydeliorman <dlrmn.akay@gmail.com>
2023-05-20 19:21:39 +0300

    project updated

---
+++ homework2/src/main/java/com/example/homework2/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.homework2.repository;
+
+import com.example.homework2.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    @Query(
+            "select user from User user where user.username= :username")
+    User findByUsername(String username);
+
+    @Query("SELECT user FROM User user WHERE user.username = :username AND user.phoneNumber = :phoneNumber")
+    User findByUsernameAndPhoneNumber(@Param("username")String username, @Param("phoneNumber") String phoneNumber);
+}
